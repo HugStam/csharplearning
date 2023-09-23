0e8bbce71bbe4134934afe1e9af51edea71f3cd9
Dexter Ty <dexter.gets@gmail.com>
2023-09-23 10:37:55 -0400

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace consoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+             var random = new Random();
+             for (var i = 0; i < 10; i++) 
+                Console.Write((char)('a' + random.Next(0,26)));
+            
+        }
+    
+    }
+
+
+
+}
